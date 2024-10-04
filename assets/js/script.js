3d54a64d6c9daad5be8676fe1b37d860ce2bbc42
Marzalado-feutech <202310689@fit.edu.ph>
2024-10-04 14:56:22 +0800

    test2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,28 @@
-const imageContainer = document.getElementById('image-container');
+const image = document.getElementById('image');
+let x = 0;
+let y = 0;
+let speed = 5;
+const maxWidth = window.innerWidth - image.offsetWidth;
+const maxHeight = window.innerHeight - image.offsetHeight;
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 500){
-        imageContainer.classList.add('animate');
-    } else {
-        imageContainer.classList.remove('animate');
+document.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'a':
+            x -= speed;
+            if (x < 0) x = 0;
+            break;
+        case 'd':
+            x += speed;
+            if (x > maxWidth) x = maxWidth;
+            break;
+        case 'w':
+            y -= speed;
+            if (y < 0) y = 0;
+            break;
+        case 's':
+            y += speed;
+            if (y > maxHeight) y = maxHeight;
+            break;
     }
+    image.style.transform = `translate(${x}px, ${y}px)`;
 });
